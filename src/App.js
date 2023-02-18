e36994ee811a71fa100e9e0dfdfb2862c54799c0
sonaminku <110347555+sonaminku@users.noreply.github.com>
2023-02-18 20:50:30 +0530

    Updated my cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <img src="/img6.jpg" alt="" width={1550} className="max-w-full h-auto" />
+      <img src="https://previews.123rf.com/images/gioiak2/gioiak22101/gioiak2210100106/162310369-eshop-ecommerce-digital-marketing-web-page-on-a-laptop-computer-screen-office-desk-background-online.jpg" alt="" width={1550} height={800} className="max-w-full h-auto" />
     </div>
   )
 }
